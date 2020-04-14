f73ddc631d38d5c70258465659d2aa9699026075
guy <guy120494@gmail.com>
2020-04-14 11:43:49 +0300

    Added groupy

---
+++ groupy/groupy/gfunc/plot/plot_z2.py
@@ -0,0 +1,16 @@
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_z2(f, fignum=None, range=None, color_map='gray'):
+    # plt.figure(fignum)
+
+    if range is None:
+        plt.imshow(f, interpolation='nearest', cmap=cm.get_cmap(color_map))
+    else:
+        plt.imshow(f, interpolation='nearest', cmap=cm.get_cmap(color_map),
+                   vmin=range[0], vmax=range[1])
+
+    plt.xticks(np.arange(f.shape[1]), [str(i) for i in np.arange(f.shape[1])])
+    plt.yticks(np.arange(f.shape[0]), [str(i) for i in np.arange(f.shape[0])])
