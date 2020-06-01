9b8ee909c2d7ebe5925f37f46048083d33137e4c
guy <guy120494@gmail.com>
2020-06-01 21:02:15 +0300

    Make max pooling invariant to rotations

--- models/P4Model.py
+++ models/P4Model.py
@@ -10,6 +10,11 @@ def plane_group_spatial_max_pooling(x, group):
         x = tf.reshape(x, [x_shape[0], x_shape[1], x_shape[2], -1, 1])
     elif group == 'C4':
         x = tf.reshape(x, [x_shape[0], x_shape[1], x_shape[2], -1, 4])
+        x = tf.unstack(x, axis=-1)
+        x[0] = tf.image.rot90(x[0], 3)
+        x[1] = tf.image.rot90(x[1], 2)
+        x[2] = tf.image.rot90(x[2], 1)
+        x = tf.stack(x, axis=-1)
     else:  # The group is D4
         x = tf.reshape(x, [x_shape[0], x_shape[1], x_shape[2], -1, 8])
     return tf.reduce_max(x, axis=[4])
