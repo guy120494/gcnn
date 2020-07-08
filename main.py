47a1ce58382261503f762ee71692ee463cb1554d
guy <guy120494@gmail.com>
2020-07-08 14:25:14 +0300

    Test invariant vs equivariant pooling with non rotated train

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import tensorflow as tf
 from numpy import newaxis
 from tensorflow import keras
 
-from models.MnistModel import MnistModel
 from models.P4Model import P4Model
 from models.P4ModelInvariantMaxPooling import P4ModelInvariantMaxPooling
-from models.Z2Model import Z2Model
 
 
 def get_mnist_data() -> Tuple[Any, Any, Any, Any]:
@@ -114,17 +112,22 @@ if __name__ == '__main__':
     # train_model(p4_model_invariant_max_pooling, train_dataset, rotate_train=True)
     # test_model(p4_model_invariant_max_pooling, rotate_test=True, test_dataset=test_dataset)
 
-    print("\n----- P4 MODEL EQUIVARIANT POOLING-----\n")
+    print("\n----- P4 MODEL EQUIVARIANT POOLING NOT ROTATED TRAIN-----\n")
     p4_model = P4Model()
-    train_model(p4_model, train_dataset, rotate_train=True)
+    train_model(p4_model, train_dataset)
     test_model(p4_model, rotate_test=True, test_dataset=test_dataset)
 
-    print("\n----- Z2 MODEL -----\n")
-    z2_model = Z2Model()
-    train_model(z2_model, train_dataset, rotate_train=True)
-    test_model(z2_model, rotate_test=True, test_dataset=test_dataset)
-
-    print("\n----- MNIST MODEL -----\n")
-    mnist_model = MnistModel()
-    train_model(mnist_model, train_dataset, rotate_train=True)
-    test_model(mnist_model, rotate_test=True, test_dataset=test_dataset)
+    # print("\n----- P4 MODEL EQUIVARIANT POOLING-----\n")
+    # p4_model = P4Model()
+    # train_model(p4_model, train_dataset, rotate_train=True)
+    # test_model(p4_model, rotate_test=True, test_dataset=test_dataset)
+    #
+    # print("\n----- Z2 MODEL -----\n")
+    # z2_model = Z2Model()
+    # train_model(z2_model, train_dataset, rotate_train=True)
+    # test_model(z2_model, rotate_test=True, test_dataset=test_dataset)
+    #
+    # print("\n----- MNIST MODEL -----\n")
+    # mnist_model = MnistModel()
+    # train_model(mnist_model, train_dataset, rotate_train=True)
+    # test_model(mnist_model, rotate_test=True, test_dataset=test_dataset)
