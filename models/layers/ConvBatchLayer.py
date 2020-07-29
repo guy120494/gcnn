6b34ea252a47bd1e79bff77dfc358b5d6034a8a5
guy <guy120494@gmail.com>
2020-07-29 14:45:37 +0300

    Add a default activation

--- models/layers/ConvBatchLayer.py
+++ models/layers/ConvBatchLayer.py
@@ -1,12 +1,16 @@
+import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow_core.python.layers.base import Layer
 
 
 class ConvBatchLayer(layers.Layer):
-    def __init__(self, conv, **kwargs):
+    def __init__(self, conv: Layer, **kwargs):
         super(ConvBatchLayer, self).__init__(**kwargs)
         self.batch = layers.BatchNormalization()
         self.conv = conv
 
     def call(self, inputs, training=None):
         x = self.conv(inputs)
+        if not hasattr(x, 'activation'):
+            x = tf.nn.relu(x)
         return self.batch(x, training)
