e5c6798c8268d424756f46237d43ebf2da66080a
guy <guy120494@gmail.com>
2020-07-29 14:46:26 +0300

    p4m also invariant (hopefully)

--- models/layers/InvariantPoolingLayer.py
+++ models/layers/InvariantPoolingLayer.py
@@ -23,4 +23,14 @@ class InvariantPoolingLayer(layers.Layer):
             x = tf.stack(x, axis=-1)
         else:  # The group is D4
             x = tf.reshape(inputs, [x_shape[0], x_shape[1], x_shape[2], -1, 8])
+            x = tf.unstack(x, axis=-1)
+            x[1] = tf.image.rot90(x[1], 3)
+            x[2] = tf.image.rot90(x[2], 2)
+            x[3] = tf.image.rot90(x[3], 1)
+            x[4] = tf.image.flip_left_right(x[4])
+            x[5] = tf.image.flip_left_right(tf.image.rot90(x[5], 3))
+            x[6] = tf.image.flip_left_right(tf.image.rot90(x[6], 2))
+            x[7] = tf.image.flip_left_right(tf.image.rot90(x[7], 1))
+            x = tf.stack(x, axis=-1)
+
         return tf.reduce_max(x, axis=[4])
