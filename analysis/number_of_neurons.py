262365524d689d2373a2987c73c2eeec172f808a
guy <guy120494@gmail.com>
2020-10-06 17:06:05 +0300

    Unify more files

--- analysis/number_of_neurons.py
+++ analysis/number_of_neurons.py
@@ -11,10 +11,16 @@ def unify_equivariant_files():
     return pd.concat([first_file, second_file, third_file]).drop(columns=["Unnamed: 0"])
 
 
+def unify_invariant_files():
+    first_file = pd.read_csv('/Users/guy/Desktop/gcnn/number-of-neurons/invariant-model-500-to-1000.csv')
+    second_file = pd.read_csv('/Users/guy/Desktop/gcnn/number-of-neurons/invariant-model-1000-to-1500.csv')
+
+    return pd.concat([first_file, second_file]).drop(columns=["Unnamed: 0"])
+
+
 def unify_files():
     equivariant_file = unify_equivariant_files()
-    invariant_file = pd.read_csv('/Users/guy/Desktop/gcnn/number-of-neurons/invariant-model-number-of-neurons.csv') \
-        .drop(columns=["Unnamed: 0"])
+    invariant_file = unify_invariant_files()
 
     unified_file = pd.concat({"invariant": invariant_file, "equivariant": equivariant_file})
     unified_file.to_csv("unified.csv", index_label=["model", "row"])
