d1e8120e001d9162cb526308f828c4af09227a9d
guy <guy120494@gmail.com>
2021-01-28 12:22:44 +0200

    Added last two dropouts

--- models/cifar10/DenseInvariantModel.py
+++ models/cifar10/DenseInvariantModel.py
@@ -12,7 +12,7 @@ class DenseInvariantModel(tf.keras.Model):
         self.conv2 = GroupConv(input_gruop='C4', output_group='C4', input_channels=16, output_channels=32, ksize=3)
         self.relu2 = tf.keras.layers.ReLU()
         self.max1 = tf.keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='SAME')
-        self.drop1 = tf.keras.layers.Dropout(rate=0.25)
+        # self.drop1 = tf.keras.layers.Dropout(rate=0.25)
         self.conv3 = GroupConv(input_gruop='C4', output_group='C4', input_channels=32, output_channels=64, ksize=3)
         self.relu3 = tf.keras.layers.ReLU()
         self.max2 = tf.keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='SAME')
@@ -36,11 +36,11 @@ class DenseInvariantModel(tf.keras.Model):
         x = self.max2(x)
         x = self.conv4(x)
         x = self.relu4(x)
-        # x = self.drop2(x, training=training)
+        x = self.drop2(x, training=training)
         x = self.dense1(x)
         x = self.relu4(x)
         x = self.max_pooling(x, group='C4')
-        # x = self.drop3(x, training=training)
+        x = self.drop3(x, training=training)
 
         return self.dense2(x)
 
